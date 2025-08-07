262f7731751f3ed5c79ea74911e692a1385a47b8
Tadasei <chemselhak.mahouche@gmail.com>
2025-08-07 16:20:53 +0100

    added state methods for the type attribute to ContactMethodFactory

--- stubs/database/factories/ContactMethodFactory.php
+++ stubs/database/factories/ContactMethodFactory.php
@@ -6,7 +6,10 @@ use App\Models\Contact;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Nnjeim\World\Models\Country;
 
-use App\Enums\{ContactMethodType, PhoneValueRegex};
+use App\Enums\{
+	ContactMethodType,
+	PhoneValueRegex
+};
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ContactMethod>
@@ -63,5 +66,85 @@ class ContactMethodFactory extends Factory
 
 			"value" => $value,
 		];
+	}
+
+	/**
+	 * Indicate that the contact method is of url type.
+	 */
+	public function url(): Factory
+	{
+		return $this->state(
+			fn(array $attributes) => [
+				"type" => ContactMethodType::Url->value,
+
+				"country_id" => null,
+
+				"value" => fake()->url(),
+			],
+		);
+	}
+
+	/**
+	 * Indicate that the contact method is of email type.
+	 */
+	public function email(): Factory
+	{
+		return $this->state(
+			fn(array $attributes) => [
+				"type" => ContactMethodType::Email->value,
+
+				"country_id" => null,
+
+				"value" => fake()->email(),
+			],
+		);
+	}
+
+	/**
+	 * Indicate that the contact method is of landline type.
+	 */
+	public function landline(): Factory
+	{
+		return $this->state(
+			fn(array $attributes) => [
+				"type" => ContactMethodType::Landline->value,
+
+				"country_id" => Country::inRandomOrder()->first()->id,
+
+				"value" => fake()->regexify(PhoneValueRegex::Landline->value),
+			],
+		);
+	}
+
+	/**
+	 * Indicate that the contact method is of mobile type.
+	 */
+	public function mobile(): Factory
+	{
+		return $this->state(
+			fn(array $attributes) => [
+				"type" => ContactMethodType::Mobile->value,
+
+				"country_id" => Country::inRandomOrder()->first()->id,
+
+				"value" => fake()->regexify(PhoneValueRegex::Mobile->value),
+			],
+		);
+	}
+
+	/**
+	 * Indicate that the contact method is of fax type.
+	 */
+	public function fax(): Factory
+	{
+		return $this->state(
+			fn(array $attributes) => [
+				"type" => ContactMethodType::Fax->value,
+
+				"country_id" => Country::inRandomOrder()->first()->id,
+
+				"value" => fake()->regexify(PhoneValueRegex::Landline->value),
+			],
+		);
 	}
 }
